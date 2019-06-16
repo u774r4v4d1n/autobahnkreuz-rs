5689860db84116a1e4512edcb2427db63a83b0f2
Fin Christensen <christensen.fin@gmail.com>
2019-06-16 14:09:50 +0200

    Adjust scenario to work with 5 nodes

--- scripts/scenarios.sh
+++ scripts/scenarios.sh
@@ -14,9 +14,13 @@ tmux new-session -d -s "$1" \
      "RUST_LOG=$RUST_LOG RUST_BACKTRACE=$RUST_BACKTRACE NODE_ID=0 NODE_ADDRESS=localhost:1330 NODE_GATEWAY=$GATEWAY WAMP_ADDRESS=0.0.0.0:8090 cargo run"
 tmux select-window -t "$1:1"
 sleep 1
-tmux split-window -v -t 1 \
-     "RUST_LOG=$RUST_LOG RUST_BACKTRACE=$RUST_BACKTRACE NODE_ID=1 NODE_ADDRESS=localhost:1331 NODE_GATEWAY=$GATEWAY WAMP_ADDRESS=0.0.0.0:8091 cargo run"
 tmux split-window -h -t 1 \
+     "RUST_LOG=$RUST_LOG RUST_BACKTRACE=$RUST_BACKTRACE NODE_ID=1 NODE_ADDRESS=localhost:1331 NODE_GATEWAY=$GATEWAY WAMP_ADDRESS=0.0.0.0:8091 cargo run"
+tmux split-window -v -t 1 \
      "RUST_LOG=$RUST_LOG RUST_BACKTRACE=$RUST_BACKTRACE NODE_ID=2 NODE_ADDRESS=localhost:1332 NODE_GATEWAY=$GATEWAY WAMP_ADDRESS=0.0.0.0:8092 cargo run"
-tmux split-window -h -t 3 -c "scenarios/$1" "zsh"
+tmux split-window -v -t 1 \
+     "RUST_LOG=$RUST_LOG RUST_BACKTRACE=$RUST_BACKTRACE NODE_ID=3 NODE_ADDRESS=localhost:1333 NODE_GATEWAY=$GATEWAY WAMP_ADDRESS=0.0.0.0:8093 cargo run"
+tmux split-window -v -t 4 -c "scenarios/$1" "zsh"
+tmux split-window -v -t 4 \
+     "RUST_LOG=$RUST_LOG RUST_BACKTRACE=$RUST_BACKTRACE NODE_ID=4 NODE_ADDRESS=localhost:1334 NODE_GATEWAY=$GATEWAY WAMP_ADDRESS=0.0.0.0:8094 cargo run"
 tmux -2 attach-session -t "$1"
